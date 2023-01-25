cc0eb505a401596e8b4602185d784335aa6148fe
username <youremail@domain.com>
2023-01-25 13:22:00 -0800

    added schema

--- utils/users-api.js
+++ utils/users-api.js
@@ -4,10 +4,9 @@ const sendRequest = require('./send-request')
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 const siteId = process.env.SITE_ID;
 
-// Function to get your site by name
 const getSiteByName = async (siteId) => {
 
-    const sitesUrl = `https://api.webflow.com/sites/${siteId}/webhooks`
+    const sitesUrl = `https://api.webflow.com/sites/${siteId}`
     const options = {
         method: 'GET',
         headers: {
@@ -15,40 +14,52 @@ const getSiteByName = async (siteId) => {
             authorization: `Bearer ${ACCESS_TOKEN}`
         }
     };
+    const res = await fetch(sitesUrl, options)
+    if (res.ok) return res.json()
+}
+const getSiteUsers = async (siteId) => {
 
-    const sites = await fetch(sitesUrl, options)
-        .then(res => res.json())
-        .catch(err => console.error('error:' + err));
-    return sites
+    const sitesUrl = `https://api.webflow.com/sites/${siteId}/users`
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            authorization: `Bearer ${ACCESS_TOKEN}`
+        }
+    };
+    const res = await fetch(sitesUrl, options)
+    if (res.ok) return res.json()
+}
+
+
+const newMemberHook = async (siteId) => {
+    const siteEndpoint = `https://api.webflow.com/sites/${siteId}/webhooks`
+    const options = {
+        method: 'POST',
+        headers: {accept: 'application/json', 'content-type': 'application/json'},
+        body: JSON.stringify({triggerType: 'form_submission'})
+      };
+      const res = await fetch(siteEndpoint, options)
+      if (res.ok) return res.json();
+}
+const listWebHooks = async (siteId) => {
+    const siteEndpoint = `https://api.webflow.com/sites/${siteId}/webhooks`
+    const options = {
+        method: 'GET',
+        headers: {accept: 'application/json'},
+      };
+      const res = await fetch(siteEndpoint, options)
+      if (res.ok) return res.json();
 }
 
 // Run the function and return the Site ID
 (async () => {
-    const site = await getSiteByName(siteId)
-console.log(site)
-    // site.users.forEach((user) => console.log(user.data))
-
+    const sites = await getSiteByName(siteId);
+    const users = await getSiteUsers(siteId)
+    const hook = await listWebHooks(siteId)
+    const newMember = await newMemberHook(siteId)
+    // users.users.forEach(user => console.log(user)) 
+    console.log(hook, "hooks", newMember, "new member")
 })();
 
 
-
-// const url = 'https://api.webflow.com/sites/639751350108e54a68d9a1ae/users';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     authorization: 'Bearer '
-//   }
-// };
-
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => json)
-//   .then((data) => {
-//       console.log("I am here",data.users)
-//       // data.forEach((user) => {
-//       //   console.log(user)
-//       // });
-//   })
-//   .catch(err => console.error('error:' + err));
